c4b50e6fb5427500b1e045a3766e945553ba5424
OmaRmz <omar29957@gmail.com>
2024-09-10 11:57:55 -0600

    1st commit
    Basic Laravel Knowledge

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -15,9 +15,12 @@ class UserController extends Controller
 
     public function check_update(string $name, string $email)
     {
-        // TASK: find a user by $name and update it with $email
-        //   if not found, create a user with $name, $email and random password
-        $user = NULL; // updated or created user
+        $user = User::updateOrCreate([ 
+            'name' => $name,
+        ], [
+            'email' => $email,
+            'password' => str()->password(),
+        ]); 
 
         return $user->name;
     }
